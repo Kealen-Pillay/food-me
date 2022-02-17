8cf5d0d8c7d2c8a313d57b18bedb4e65dd42177c
Kealen-Pillay <kealenpillay@gmail.com>
2022-02-17 14:03:15 +1300

    Added ingredients dialog box

--- src/Components/FoodCard.tsx
+++ src/Components/FoodCard.tsx
@@ -6,8 +6,12 @@ import {
   CardMedia,
   Grid,
   Typography,
+  Dialog,
+  DialogTitle,
+  DialogContent,
 } from "@mui/material";
 import { createStyles, makeStyles } from "@mui/styles";
+import { useState } from "react";
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -29,46 +33,68 @@ const useStyles = makeStyles(() =>
     },
   })
 );
+
 interface Props {
   recipeObj: any;
 }
 
 export const FoodCard = ({ recipeObj }: Props) => {
+  console.log(recipeObj.recipe);
   const classes = useStyles();
-  return (
-    <Card
-      sx={{ maxWidth: 345, backgroundColor: "#1E1E1E", color: "white" }}
-      className={classes.card}
-    >
-      <CardHeader title={recipeObj.recipe.label} titleTypographyProps={{variant:'h6' }}/>
-      <CardContent className={classes.media}>
-        <CardMedia
-          component="img"
-          height="194"
-          image={recipeObj.recipe.image}
-        />
-      </CardContent>
+  const [open, setOpen] = useState(false);
 
-      <CardContent>
-        <Grid
-          container
-          alignItems="center"
-          direction="column"
-          justifyContent="space-between"
-          spacing={1}
-        >
-          <Grid item>
-            <Button className={classes.buttons}>
-              <Typography>Ingredients</Typography>
-            </Button>
+  return (
+    <>
+      <Dialog open={open}>
+        <DialogTitle>Ingredients</DialogTitle>
+        <DialogContent>{
+          recipeObj.recipe.ingredients.map((ingredient:any) => {
+            return <Typography>
+              {ingredient.text}
+            </Typography>
+          })}</DialogContent>
+        <Button onClick={() => setOpen(false)}>Close</Button>
+      </Dialog>
+      <Card
+        sx={{ maxWidth: 345, backgroundColor: "#1E1E1E", color: "white" }}
+        className={classes.card}
+      >
+        <CardHeader
+          title={recipeObj.recipe.label}
+          titleTypographyProps={{ variant: "h6" }}
+        />
+        <CardContent className={classes.media}>
+          <CardMedia
+            component="img"
+            height="194"
+            image={recipeObj.recipe.image}
+          />
+        </CardContent>
+
+        <CardContent>
+          <Grid
+            container
+            alignItems="center"
+            direction="column"
+            justifyContent="space-between"
+            spacing={1}
+          >
+            <Grid item>
+              <Button className={classes.buttons} onClick={() => setOpen(true)}>
+                <Typography>Ingredients</Typography>
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                className={classes.buttons}
+                onClick={() => window.open(recipeObj.recipe.url)}
+              >
+                <Typography>Recipe</Typography>
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item>
-            <Button className={classes.buttons} onClick={() => window.open(recipeObj.recipe.url)}>
-              <Typography>Recipe</Typography>
-            </Button>
-          </Grid>
-        </Grid>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
+    </>
   );
 };
